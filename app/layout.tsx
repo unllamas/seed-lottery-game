2aa4947871712403ca11dfeba77cc662b72591b5
Jona <llamasjonat@gmail.com>
2025-04-25 11:57:08 -0300

    feat: integrate google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,36 @@
-import type React from "react"
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from 'react';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+import Script from 'next/script';
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Bitcoin Telequino",
-  description: "Juega a la lotería Bitcoin y gana satoshis al instante",
-    generator: 'v0.dev'
-}
+  title: 'Bitcoin Telequino',
+  description: 'Juega a la lotería Bitcoin y gana satoshis al instante',
+  generator: 'v0.dev',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es" suppressHydrationWarning>
+    <html lang='es' suppressHydrationWarning>
+      <head>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}`} />
+        <Script id='google-analytics'>
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}');
+      `}
+        </Script>
+      </head>
       <body className={`${inter.className} dark`}>
-        <ThemeProvider defaultTheme="dark">{children}</ThemeProvider>
+        <ThemeProvider defaultTheme='dark'>{children}</ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
