b4afbeae51bb097086fe6a663942ea9508ab30ae
Jona <llamasjonat@gmail.com>
2025-04-26 00:24:20 -0300

    fix: changed og metadata

---
+++ config/site.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  name: 'Seed Lottery Game',
+  description: 'Find out if your seed is legendary!',
+  url: 'https://seed-lottery-game.vercel.app/',
+  ogImage: 'https://seed-lottery-game.vercel.app/og-image.png',
+};
